83f7fc26a9af185b602d4f5b9be3562aed9c4bde
Enrico Cervato <e.cervato@comparegroup.eu>
2015-05-14 21:26:33 +0200

    first commit

---
+++ Assets/scripts/Laser.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+
+	private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+
+		if (Physics.Raycast (transform.position, transform.forward, out hit)) {
+			if (hit.collider) {
+				lineRenderer.SetPosition (1, new Vector3 (0, 0, hit.distance));
+			}
+		} else {
+			lineRenderer.SetPosition (1, new Vector3 (0, 0, 5000));
+		}
+	}
+}
---
+++ Assets/scripts/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	public GameObject gunBarrel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Fire();
+		}
+	}
+
+	void Fire() {
+
+		RaycastHit hit;
+		if (Physics.Raycast (gunBarrel.transform.position, gunBarrel.transform.forward, out hit)) {
+			if(hit.collider.tag == "Enemy")
+			{
+				print ("Enemy is hit");
+			}
+		}
+	}
+}
