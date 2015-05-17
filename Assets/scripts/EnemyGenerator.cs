188b9958a36b18c8df5d4de28740df1417e45448
Enrico Cervato <e.cervato@comparegroup.eu>
2015-05-17 10:24:03 +0200

    Camera from top. Enemy generation.

---
+++ Assets/scripts/EnemyGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGenerator : MonoBehaviour {
+
+	public GameObject enemy;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Instantiate (enemy, new Vector3 (2, 0.5f, 2), Quaternion.identity);
+	}
+}
