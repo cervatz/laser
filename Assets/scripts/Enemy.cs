e28785bd00405eb9cfeb879c43e3b5ce0f2cb545
Enrico Cervato <e.cervato@comparegroup.eu>
2015-05-24 21:54:56 +0200

    Enemies now going towards the player.

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
+	public float enemySpeed;
+
+	private GameObject player;
+
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.Find ("Player");	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.position = Vector3.MoveTowards(transform.position, player.transform.position, enemySpeed*Time.deltaTime);
 	}
 }
