a5da490528781edee1950a04cf2bf6a3b47e70b5
Enrico Cervato <e.cervato@comparegroup.eu>
2015-06-04 18:08:12 +0200

    Deleting useless scripts and commenting out keyboard input.

--- Assets/scripts/Controls.cs
+++ Assets/scripts/Controls.cs
@@ -12,48 +12,46 @@ public class Controls : MonoBehaviour {
 	private float yDeg;
 	private Quaternion fromRotation;
 	private Quaternion toRotation;
-
-	// Use this for initialization
+	
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		PerformRotation ();
-		MoveBackAndForth ();
-	}
 
-	void MoveBackAndForth() {
-		if (Input.GetAxis("Vertical")!=0) {
-			
-			int direction = 0;
-			if(Input.GetAxis("Vertical") > 0) {
-				direction = 1;
-			} else if(Input.GetAxis("Vertical") < 0) {
-				direction = -1;
-			}
-
-			transform.Translate(Vector3.forward * Time.deltaTime * direction * movementSpeed);
-		}
-	}
-
-	void PerformRotation() {
-		if (Input.GetAxis("Horizontal")!=0) {
-			
-			int direction = 0;
-			if(Input.GetAxis("Horizontal") > 0) {
-				direction = 1;
-			} else if(Input.GetAxis("Horizontal") < 0) {
-				direction = -1;
-			}
-
-			xDeg += direction * rotationSpeed * friction;
-			fromRotation = transform.rotation;
-			toRotation = Quaternion.Euler(0, xDeg, 0);
-			
-			transform.rotation = Quaternion.Lerp(fromRotation, toRotation, lerpSpeed);
-			
-		}
-	}
+//	void Update () {
+//		PerformRotation ();
+//		MoveBackAndForth ();
+//	}
+//
+//	void MoveBackAndForth() {
+//		if (Input.GetAxis("Vertical")!=0) {
+//			
+//			int direction = 0;
+//			if(Input.GetAxis("Vertical") > 0) {
+//				direction = 1;
+//			} else if(Input.GetAxis("Vertical") < 0) {
+//				direction = -1;
+//			}
+//
+//			transform.Translate(Vector3.forward * Time.deltaTime * direction * movementSpeed);
+//		}
+//	}
+//
+//	void PerformRotation() {
+//		if (Input.GetAxis("Horizontal")!=0) {
+//			
+//			int direction = 0;
+//			if(Input.GetAxis("Horizontal") > 0) {
+//				direction = 1;
+//			} else if(Input.GetAxis("Horizontal") < 0) {
+//				direction = -1;
+//			}
+//
+//			xDeg += direction * rotationSpeed * friction;
+//			fromRotation = transform.rotation;
+//			toRotation = Quaternion.Euler(0, xDeg, 0);
+//			
+//			transform.rotation = Quaternion.Lerp(fromRotation, toRotation, lerpSpeed);
+//			
+//		}
+//	}
 }
